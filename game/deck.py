385ef1a8be2c727723bde350b096dcaa1e65e8ba
Tami Gartner <31107704+tagaertner@users.noreply.github.com>
2023-09-10 22:12:24 -0400

    worked on deck.py create deck, assing values to cards, shuffle deck. deleted json file not needed.

--- game/deck.py
+++ game/deck.py
@@ -1,2 +1,33 @@
-'''moudle that defines the deckl class and it functioanls such as shuffling and dealing cards
-'''
+import random
+
+
+# Define ranks and suits of cards
+ranks = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
+suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+cards =[]
+
+# Create a deck of cards
+def deck_cards():
+    for rank in ranks:
+        for suit in suits:
+          cards.append([rank, suit])
+          
+  #assign number value to face cards
+    if "Ace" == 11:
+      value = 11
+    elif "Jack"== 10 or "Quenn"== 10 or "King"== 10:
+      value = 10
+    else:
+      value = rank 
+        
+#deck = [(rank, suit) for rank in ranks for suit in suits]
+# Shuffle the deck
+    random.shuffle(cards)
+
+# Print the shuffled deck
+    for card in cards:
+        print(f"{card[0]} of {card[1]}")
+
+def deal_cards():
+
+  deal_cards()
--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-import json
-# read the contens of the data.json file
-with open('data/data.json') as file:
-  data = json.load(file)
-import banking.player_account
-import banking.dealer_account
+'''main to handle the entry point and user inputs and will orchestrates the execution flow
+ and coordinates interactions between different commponents '''
 
+from game.deck import deck_cards
 dollars = ""
 
 print("Let's play Black Jack!!")
-print(f"You have {dollars} in the bank. How much do you wish to bet: n/")
+print(f"You have {dollars} in the bank. How much do you wish to bet: \n")
+
+
+
+
+
+if __name__ == '__main__':
+    deck_cards()
+    
