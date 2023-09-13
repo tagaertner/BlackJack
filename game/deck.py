1dba9655adecd33a80ee9c4ee49413323f3ab978
Tami Gartner <31107704+tagaertner@users.noreply.github.com>
2023-09-13 00:03:03 -0400

    rewrote deck.py

--- game/deck.py
+++ game/deck.py
@@ -1,33 +1,59 @@
 import random
 
 
-# Define ranks and suits of cards
+# Define ranks and suits
 ranks = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
 suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
 cards =[]
 
 # Create a deck of cards
-def deck_cards():
-    for rank in ranks:
-        for suit in suits:
-          cards.append([rank, suit])
-          
-  #assign number value to face cards
-    if "Ace" == 11:
-      value = 11
-    elif "Jack"== 10 or "Quenn"== 10 or "King"== 10:
-      value = 10
-    else:
-      value = rank 
-        
-#deck = [(rank, suit) for rank in ranks for suit in suits]
+for rank in ranks:
+  for suit in suits:
+    cards.append([rank, suit])
+
+ # Assign number value to cards
+def get_card_vaule(rank, current_total):
+  if rank == "Ace":
+    #if adding to 11 does not bust, treat as 11 else treat as 1
+      return 11 if current_total + 11 <= 21 else 1 
+  elif rank in ["Jack", "Queen", "King"]:
+      return 10
+  else:
+      return int(rank)
+    
 # Shuffle the deck
-    random.shuffle(cards)
+random.shuffle(cards)
 
-# Print the shuffled deck
-    for card in cards:
-        print(f"{card[0]} of {card[1]}")
+#dealing the cards
+def deal(number):
+  dealt_cards =[]
+  for card_index in range(number):
+    if len(cards) > 0:
+      dealt_card = cards.pop()
+      dealt_cards.append(dealt_card)
+    else:
+      print("no more cards in the deck")
+  return dealt_cards
 
-def deal_cards():
+# Deal 2 cards
+user_hand = deal(2)
+dealer_hand = deal(2)
 
-  deal_cards()
+# Print the hands the loop iterates over each card in both hands
+print("User's hand:")
+user_hand_total = 0
+for card in user_hand:
+  rank = card[0]
+  value = get_card_vaule(rank, user_hand_total)
+  user_hand_total += value 
+  print(f"{card[0]} of {card[1]}")
+
+print(f"Total vaule: {user_hand_total}")
+
+#dealer's hand 
+print("\nDealer's hand:")
+dealer_hand_total = 0
+print("This card face down")
+for card in dealer_hand[1:]: # used the slice method, iterate over each card and exclued the first card
+  print(f"{card[0]} of {card[1]}")
+
--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 '''main to handle the entry point and user inputs and will orchestrates the execution flow
  and coordinates interactions between different commponents '''
 
-from game.deck import deck_cards
+from game.deck import deal
 dollars = ""
 
 print("Let's play Black Jack!!")
+input = ("Press 1 to play or 2 to quit")
 print(f"You have {dollars} in the bank. How much do you wish to bet: \n")
 
+# Call the deal() function
+user_hand = deal(2)
+dealer_hand = deal(2)
 
 
-
-
-if __name__ == '__main__':
-    deck_cards()
-    
+if __name__ == '__main__':   
+    deal()
